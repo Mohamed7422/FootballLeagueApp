0d9c4356048f56b6afdc3ca69ba70733559b66c4
moham <mohamedhesen0@gmail.com>
2024-01-07 21:40:29 +0200

    Updating project

--- app/src/main/java/com/example/footballleagueapp/di/AppModule.kt
+++ app/src/main/java/com/example/footballleagueapp/di/AppModule.kt
@@ -30,12 +30,12 @@ object AppModule {
     @Singleton
     fun provideApiInterceptor(it: Interceptor.Chain): Response {
 
-            val originalRequest = it.request()
+        val originalRequest = it.request()
 
 
-            val newRequest = originalRequest.newBuilder()
-                .addHeader("X-Auth-Token",Constants.API_KEY)
-                .build()
+        val newRequest = originalRequest.newBuilder()
+            .addHeader("X-Auth-Token", Constants.API_KEY)
+            .build()
 
         return it.proceed(newRequest)
 
@@ -44,55 +44,55 @@ object AppModule {
 
     @Provides
     @Singleton
-     fun provideOkHttpClient():OkHttpClient{
+    fun provideOkHttpClient(): OkHttpClient {
 
-        val certificatePinner  =  CertificatePinner.Builder()
-            .add("api.football-data.org","sha256/${Constants.SHA_KEY}")
+        val certificatePinner = CertificatePinner.Builder()
+            .add("api.football-data.org", "sha256/${Constants.SHA_KEY}")
             .build()
 
 
         return OkHttpClient.Builder()
             .certificatePinner(certificatePinner)
-             .addInterceptor {
-                 chain ->  provideApiInterceptor(chain)
-             }.build()
-     }
-
-    @Provides
-    @Singleton
-     fun providesCompetitionsApiClient(): CompetitionsApiService {
-         return Retrofit.Builder()
-             .client(provideOkHttpClient())
-             .baseUrl(Constants.BASE_URL)
-             .addConverterFactory(GsonConverterFactory.create())
-             .build()
-             .create(CompetitionsApiService::class.java)
-     }
-
-
-    @Provides
-    @Singleton
-    fun providesCompetitionsDataBase(@ApplicationContext context:Context):CompetitionsDataBase{
-        return Room.databaseBuilder(context,
-            CompetitionsDataBase::class.java,"competitionsDataBase.dp").build()
+            .addInterceptor { chain ->
+                provideApiInterceptor(chain)
+            }.build()
     }
 
     @Provides
-    fun providesCompetitionsDao(competitionsDataBase: CompetitionsDataBase):CompetitionDao{
+    @Singleton
+    fun providesCompetitionsApiClient(): CompetitionsApiService {
+        return Retrofit.Builder()
+            .client(provideOkHttpClient())
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CompetitionsApiService::class.java)
+    }
+
+
+    @Provides
+    @Singleton
+    fun providesCompetitionsDataBase(@ApplicationContext context: Context): CompetitionsDataBase {
+        return Room.databaseBuilder(
+            context,
+            CompetitionsDataBase::class.java, "competitionsDataBase.dp"
+        ).build()
+    }
+
+    @Provides
+    fun providesCompetitionsDao(competitionsDataBase: CompetitionsDataBase): CompetitionDao {
         return competitionsDataBase.competitionsDao()
     }
 
 
-
-
     @Provides
     @Singleton
-    fun providesCompetitionsRepo(dao:CompetitionDao,api: CompetitionsApiService):CompetitionsRepo{
-        return CompetitionsRepoImpl(dao,api)
+    fun providesCompetitionsRepo(
+        dao: CompetitionDao,
+        api: CompetitionsApiService
+    ): CompetitionsRepo {
+        return CompetitionsRepoImpl(dao, api)
     }
-
-
-
 
 
 }
